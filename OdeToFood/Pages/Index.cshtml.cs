a68b142a40c37061ad4010c49162649a122fb383
khauminhduy <khauminhduy@gmail.com>
2022-09-18 21:43:59 +0700

    Commit

--- OdeToFood/Pages/Index.cshtml.cs
+++ OdeToFood/Pages/Index.cshtml.cs
@@ -12,6 +12,8 @@ namespace OdeToFood.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        public string Message { get; set; }
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -19,7 +21,7 @@ namespace OdeToFood.Pages
 
         public void OnGet()
         {
-
+            Message = "Welcome";
         }
     }
 }
